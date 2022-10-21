bb4bcff5415b9ae7c136de146b417c4221622f98
Jens-U. Mozdzen <jmozdzen@nde.ag>
2022-10-21 16:33:57 +0200

    cosmetic clean-up

--- plugin.php
+++ plugin.php
@@ -14,24 +14,25 @@ if( !defined( 'YOURLS_ABSPATH' ) ) die();
  
 // Generate a random keyword
 yourls_add_filter( 'random_keyword', 'afp_random_keyword' );
+
 function afp_random_keyword() {
-        // elaboration from ozh random-keywords-master plugin
-		global $afp_random_keyword;
-		
-		// Define Salt
-		$salt = 'DefineYourSaltHere'; // change your own salt
-		
-		// Set minimum character of keyword
-		$minchar = 6; // default is 6
-		
-		// get next decimal from 
-		$id = yourls_get_next_decimal();
-        $possible = yourls_get_shorturl_charset() ;
-        $str='';
-		
-		$hashids = new Hashids\Hashids($salt,$minchar); // Salt is qQZQlDX18C, min hash character is 6
-		$str = $hashids->encode($id);
-		
-        return $str;
+	// elaboration from ozh random-keywords-master plugin
+	global $afp_random_keyword;
+
+	// Define Salt
+	$salt = 'DefineYourSaltHere'; // change your own salt
+
+	// Set minimum character of keyword
+	$minchar = 6; // default is 6
+
+	// get next decimal from 
+	$id = yourls_get_next_decimal();
+	$possible = yourls_get_shorturl_charset() ;
+	$str='';
+
+	$hashids = new Hashids\Hashids($salt,$minchar); // Salt is qQZQlDX18C, min hash character is 6
+	$str = $hashids->encode($id);
+
+	return $str;
 }
 
