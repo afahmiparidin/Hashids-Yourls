2f5484ff351be66f79b6e08e531acd5eef26c6f5
Jens-U. Mozdzen <jmozdzen@nde.ag>
2022-10-21 16:41:29 +0200

    configuration now done via global parameters

--- plugin.php
+++ plugin.php
@@ -3,14 +3,25 @@
 Plugin Name: Hashids-Yourls
 Plugin URI: https://github.com/afahmiparidin/Hashids-Yourls/
 Description: To change different type of hashing keyword.
-Version: 0.1
+Version: 0.2
 Author: afahmiparidin
 Author URI: https://my.linkedin.com/in/fahmiparidin
+
+Modified by Jens-U. Mozdzen <jmozdzen@nde.ag>
+
+History:
+2022-10-21 JMo: we're now using HASHID_SALT and HASHID_MINLEN global parameters
+2022-10-21 JMo: cosmetic changes
 */
+
 require('lib/Hashids.php');
 
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+// We use two global parameters
+if( !defined( 'HASHID_SALT' ) ) die();
+if( !defined( 'HASHID_MINLEN' ) ) die();
  
 // Generate a random keyword
 yourls_add_filter( 'random_keyword', 'afp_random_keyword' );
@@ -20,17 +31,17 @@ function afp_random_keyword() {
 	global $afp_random_keyword;
 
 	// Define Salt
-	$salt = 'DefineYourSaltHere'; // change your own salt
+	$salt = HASHID_SALT;
 
 	// Set minimum character of keyword
-	$minchar = 6; // default is 6
+	$minchar = HASHID_MINLEN;
 
 	// get next decimal from 
 	$id = yourls_get_next_decimal();
 	$possible = yourls_get_shorturl_charset() ;
 	$str='';
 
-	$hashids = new Hashids\Hashids($salt,$minchar); // Salt is qQZQlDX18C, min hash character is 6
+	$hashids = new Hashids\Hashids($salt,$minchar);
 	$str = $hashids->encode($id);
 
 	return $str;
